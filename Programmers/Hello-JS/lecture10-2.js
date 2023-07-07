e94932e826ed7e9e1236bdaea2a35fb9beb5e40b
Kanon <87532431+puputia@users.noreply.github.com>
2023-07-07 14:41:22 +0900

    first commit

---
+++ Programmers/Hello-JS/lecture10-2.js
@@ -0,0 +1,5 @@
+2 * 3 > 4 + 5 && 6 / 3 == 2 || !false
+height >= 180 && gender == "male" || height >= 165 && gender == "female"
+(height >= 180 && gender == "male") || (height >= 165 && gender == "female")
+
+//결과는 같지만 두번째 코드가 가독성이 더 좋음 -> 괄호를 사용하였기 때문에 
